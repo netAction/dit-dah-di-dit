4fd1c8a02aeb9befc8a21825e3b73bd79ae7c5c7
netaction <schmidt@netaction.de>
2014-02-11 16:08:00 +0100

    Offline Cache

--- js/script.js
+++ js/script.js
@@ -240,7 +240,7 @@ function logEvent(e) {
 				// Browser downloaded a new app cache.
 				// Swap it in and reload the page to get the new hotness.
 				window.applicationCache.swapCache();
-				// if (confirm('A new version of this site is available. Load it?'))
+				if (confirm('A new version of this site is available. Load it?'))
 					window.location.reload();
 			} else {
 				// Manifest didn't changed. Nothing new to server.
