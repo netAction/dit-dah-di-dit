48107cfebb18ac32d81147b28d5837af2334e817
netaction <schmidt@netaction.de>
2014-02-11 15:57:44 +0100

    Offline Cache

--- js/script.js
+++ js/script.js
@@ -199,21 +199,27 @@ function invisibleMessage() {
 
 
 function offlineCache() {
-	// Check if a new cache is available on page load.
-	window.applicationCache.addEventListener('updateready', function(e) {
-		if (window.applicationCache.status == window.applicationCache.UPDATEREADY) {
-			// Browser downloaded a new app cache.
-			// Swap it in and reload the page to get the new hotness.
-			window.applicationCache.swapCache();
-			// if (confirm('A new version of this site is available. Load it?'))
-				window.location.reload();
-		} else {
-			// Manifest didn't changed. Nothing new to server.
-		}
+	window.addEventListener('load', function(e) {
+		// Check if a new cache is available on page load.
+		window.applicationCache.addEventListener('updateready', function(e) {
+			if (window.applicationCache.status == window.applicationCache.UPDATEREADY) {
+				// Browser downloaded a new app cache.
+				// Swap it in and reload the page to get the new hotness.
+				window.applicationCache.swapCache();
+				// if (confirm('A new version of this site is available. Load it?'))
+					window.location.reload();
+			} else {
+				// Manifest didn't changed. Nothing new to server.
+			}
+		}, false);
 	}, false);
 }
 
+// Start with the event listeners on the offline cache.
+offlineCache();
+
 $(function() {
+	// functions for the exercise types
 	newCharacters();
 	randomGroups();
 	touchGroups();
@@ -232,7 +238,5 @@ $(function() {
 		// remove all callbacks
 		morse.characterCallbacks = [];
 	});
-
-	offlineCache();
 });
 
